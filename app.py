89a2cbe8e024d1908edf10a672142efb7c8a68ac
Kellyy621 <yekelly621@gmail.com>
2025-05-16 10:15:33 -0400

    s

--- app.py
+++ app.py
@@ -14,7 +14,26 @@ singers = [
         "Name":"Clairo",
         "Birthday":"August 18, 1998",
         "Nationality":"American"
-        
 
+        "id":2
+        "Name":"Ethel Cain",
+        "Birthday":"March 24, 1998",
+        "Nationality":"American"
+
+        "id":3
+        "Name":"Phoebe Bridgers",
+        "Birthday":"August 17, 1994",
+        "Nationality":"American"
+
+        "id":4
+        "Name":"Ethel Cain",
+        "Birthday":"November 7, 1996",
+        "Nationality":"New Zealand"
     }
 ]
+@app.route('/api/singers')
+def get_singers():
+    return jsonify(singers)
+
+if __name__ == '__main__':
+    app.run(debug=True)
