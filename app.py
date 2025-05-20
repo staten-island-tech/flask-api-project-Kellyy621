c42d3295261d2f688a8ca2bb8ac394b5f05aad95
Kellyy621 <yekelly621@gmail.com>
2025-05-20 10:15:05 -0400

    s

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify
-import requests
+
 
 app = Flask(__name__)
 
@@ -10,25 +10,25 @@ singers = [
         "Name":"Clairo",
         "Birthday":"August 18, 1998",
         "Nationality":"American",
-        "Image":"clairo.jpg"
+        "Image":"https://wp.dailybruin.com/images/2024/07/Clairo-Charm-album-cover.jpeg"
     },
     {   "id":2,
         "Name":"Ethel Cain",
         "Birthday":"March 24, 1998",
         "Nationality":"American",
-        "Image":"ethel_cain.jpg"
+        "Image":"https://a2.cdn.hhv.de/items/images/generated/970x970/01208/1208222/3-ethel-cain-preacher-s-daughter.webp"
     },
     {   "id":3,
         "Name":"Phoebe Bridgers",
         "Birthday":"August 17, 1994",
         "Nationality":"American",
-        "Image":"phoebe_bridgers.jpg"
+        "Image":"https://cdn-images.dzcdn.net/images/cover/1dc007dfa7ce8eb17289a7722b98d5e9/1900x1900-000000-80-0-0.jpg"
     },
     { "id":4,
-        "Name":"Billie Ellish",
+        "Name":"Billie Eilish",
         "Birthday":"December 18, 2001",
         "Nationality":"American",
-        "Image":"billie_ellish.jpg"
+        "Image":"https://www.jambase.com/wp-content/uploads/2024/04/billie-eilish-new-album-hit-me-hard-and-soft-v2-1480x832.jpg"
     }
 ]
 @app.route('/api/singers')
