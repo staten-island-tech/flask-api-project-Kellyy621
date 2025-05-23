54f902bf690039ab3f7fdb21d56cfd4825067eca
Kellyy621 <yekelly621@gmail.com>
2025-05-23 10:15:04 -0400

    d

--- app.py
+++ app.py
@@ -1,53 +1,61 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, request, redirect, url_for 
+import requests
 
 app = Flask(__name__)
 
-#data of singers 
-singers = [
-    {
-        "id":1,
-        "Name":"Clairo",
-        "Birthday":"August 18, 1998",
-        "Nationality":"American",
-        "Songs":"Sofia, Bags, Juna",
-        "Image":"https://wp.dailybruin.com/images/2024/07/Clairo-Charm-album-cover.jpeg"
-    },
-    {   "id":2,
-        "Name":"Ethel Cain",
-        "Birthday":"March 24, 1998",
-        "Nationality":"American",
-        "Songs":"Strangers, Televangelism, American Teenager",
-        "Image":"https://a2.cdn.hhv.de/items/images/generated/970x970/01208/1208222/3-ethel-cain-preacher-s-daughter.webp"
-    },
-    {   "id":3,
-        "Name":"Phoebe Bridgers",
-        "Birthday":"August 17, 1994",
-        "Nationality":"American",
-        "Songs":"Waiting Room, Funeral, I Kmow The End, Scott Street",
-        "Image":"https://cdn-images.dzcdn.net/images/cover/1dc007dfa7ce8eb17289a7722b98d5e9/1900x1900-000000-80-0-0.jpg"
-    },
-    { "id":4,
-        "Name":"Billie Eilish",
-        "Birthday":"December 18, 2001",
-        "Nationality":"American",
-        "Songs":"Wildflower, Birds of a Feather, What Was I Made",
-        "Image":"https://www.jambase.com/wp-content/uploads/2024/04/billie-eilish-new-album-hit-me-hard-and-soft-v2-1480x832.jpg"
-    }
-]
-@app.route('/api/singers')
-def get_singers():
-    return jsonify(singers)
+#Random dog facts
+def random_dog_facts():
+    try:
+        url = f"https://dog-api.kinduff.com/api/facts?number=[number]"
+        response = requests.get(url)
+        data = response.json()
+        return data.get('facts')
+    except Exception as x:
+        print("Error getting random facts: {x}")
+        return()
+    
+def random_dog_image(breed=None):
+    try:
+        if breed:
+            url=f"https://dog.ceo/api/breed/{breed.lower()}/images/random"
+        else:
+            url="https://dog.ceo/api/breed/image/random"
+        response = request.get(url)
+        data = response.json()
+        return data.get('message')
+    
 
-@app.route('/api/singers/<int:singer_id>')
-def get_singer(singer_id):
-    singer = next((s for s in singers if s["id"] == singer_id), None)
-    if singer:
-        return jsonify(singer)
-    return jsonify({"error": "Singer not found"}), 404
-
-@app.route('/')
-def index():
-     return render_template('index.html', singers=singers)
+# New route: When a user clicks a Pokémon card, this page shows more details and a stats chart
+@app.route("/pokemon/<int:id>")
+def pokemon_detail(id):
+    # Get detailed info for the Pokémon using its id
+    response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{id}")
+    data = response.json()
+    
+    # Extract extra details like types, height, and weight
+    types = [t['type']['name'] for t in data['types']]
+    height = data.get('height')
+    weight = data.get('weight')
+    name = data.get('name').capitalize()
+    image_url = f"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{id}.png"
+    
+    # Extract base stats for the chart (e.g., hp, attack, defense, etc.)
+    stat_names = [stat['stat']['name'] for stat in data['stats']]
+    stat_values = [stat['base_stat'] for stat in data['stats']]
+    
+    # Send all details to the pokemon.html template
+    return render_template("pokemon.html", pokemon={
+        'name': name,
+        'id': id,
+        'image': image_url,
+        'types': types,
+        'height': height,
+        'weight': weight,
+        'stat_names': stat_names,
+        'stat_values': stat_values
+    })
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+    
